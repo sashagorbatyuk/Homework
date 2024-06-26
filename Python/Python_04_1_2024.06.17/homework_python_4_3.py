a525fe8f4857859acf356e976926fa9b5f8c67c1
sashagorbatyuk <sgorbatuk958@gmail.com>
2024-06-26 08:59:01 +0300

    [*] Homework Python_04_1_2024.06.17 refactoring

--- Python/Python_04_1_2024.06.17/homework_python_4_3.py
+++ Python/Python_04_1_2024.06.17/homework_python_4_3.py
@@ -7,11 +7,10 @@
 # Якщо число кратне 5, потрібно вивести слово "Buzz". Якщо число кратне 3 і 5, потрібно вивести "Fizz Buzz". 
 # Якщо число не кратне ні 3, ні 5, потрібно вивести саме число.
 
+startNumber = int(input("Enter start number of range: "))
+endNumber = int(input("Enter end number of range: "))
 
-start = int(input("Enter start of range: "))
-end = int(input("Enter end of range: "))
-
-for i in range(start, end + 1):
+for i in range(startNumber, endNumber + 1):
     if i % 3 == 0 and i % 5 == 0:
         print("Fizz Buzz")
     elif i % 3 == 0:

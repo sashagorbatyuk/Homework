6222c011d8d8b8d76bc87b906d71b39ef849ad45
sashagorbatyuk <sgorbatuk958@gmail.com>
2024-09-06 20:41:54 +0300

    [+] Added C++ folder and homework_css_2024.09.08

---
+++ С++/CSS_01_2024.09.08/Task_6/Task_6/Task_6/Task_6.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main() {
+
+    std::cout << " Hello, World!" << std::endl;
+    std::cout << "\\Dennis Ritchie\\" << std::endl;
+
+    return 0;
+}
---
+++ С++/CSS_01_2024.09.08/Task_7/Task_7/Task_7.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream> 
+
+int main() {
+
+    std::cout << "To be, or not to be: that is the question:" << std::endl;
+    std::cout << "Whether 'tis nobler in the mind to suffer" << std::endl;
+    std::cout << "The slings and arrows of outrageous fortune," << std::endl;
+    std::cout << "Or to take arms against a sea of troubles," << std::endl;
+    std::cout << "And by opposing end them?" << std::endl;
+    std::cout << "William Shakespeare" << std::endl;
+
+    return 0;
+}

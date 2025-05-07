8eb932287b886da86373b2c8ddadc6daa1299da2
sashagorbatyuk <sgorbatuk958@gmail.com>
2025-05-07 06:35:11 +0300

    [+] Added homework NP_01_2025.05.07

---
+++ NP/NP_01_2025.05.07/GoborunServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static Dictionary<string, string> responses = new();
+
+    static void Main()
+    {
+        var listener = new TcpListener(IPAddress.Any, 8080);
+        listener.Start();
+        Console.WriteLine("Сервер запущено на порту 8080...");
+
+        while (true)
+        {
+            using var client = listener.AcceptTcpClient();
+            using var stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int length = stream.Read(buffer, 0, buffer.Length);
+            string question = Encoding.UTF8.GetString(buffer, 0, length).Trim();
+
+            Console.WriteLine($"Клієнт: {question}");
+
+            string response;
+            if (responses.ContainsKey(question))
+            {
+                response = responses[question];
+            }
+            else
+            {
+                Console.Write($"Немає відповіді. Введіть відповідь для \"{question}\": ");
+                response = Console.ReadLine();
+                responses[question] = response;
+            }
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
---
+++ NP/NP_01_2025.05.07/GovorunClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        Console.WriteLine("Підключення до сервера...");
+
+        while (true)
+        {
+            Console.Write("Ви: ");
+            string message = Console.ReadLine();
+
+            using var client = new TcpClient("127.0.0.1", 8080);
+            using var stream = client.GetStream();
+
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+            byte[] buffer = new byte[1024];
+            int bytes = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+            Console.WriteLine("Сервер: " + response);
+        }
+    }
+}
